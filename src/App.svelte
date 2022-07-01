9570a3370beeaa66c82113884986af7c205bbc54
Abdullah1428 <abdullahcse1428@gmail.com>
2022-07-01 15:44:02 +0200

    started with welcome screen and some global stylings

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,15 @@
 <script>
-	
+	import Welcome from './screens/Welcome.svelte'
+
+	let state = 'welcome'; // or 'playing'
 </script>
 
 <main>
+	{#if state === 'welcome'}
+		<Welcome />
+	{:else if state === 'playing'}
+		<p>game screen goes here</p>
+	{/if}
 	
 </main>
 
@@ -10,7 +17,11 @@
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 800px;
 		margin: 0 auto;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 	}
 </style>
