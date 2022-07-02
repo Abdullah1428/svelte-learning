38beb8353d502264ff01caa64bb761fb4af2494c
Abdullah1428 <abdullahcse1428@gmail.com>
2022-07-02 19:40:54 +0200

    added game container and promises to get celebs data

--- src/screens/Game.svelte
+++ src/screens/Game.svelte
@@ -2,7 +2,19 @@
   // making the selection prop available that is passed from App.svelte
   export let selection;
 
-  console.log(selection)
+  const load_details = async (celeb) => {
+    const res = await fetch(`https://cameo-explorer.netlify.app/celebs/${celeb.id}.json`);
+
+    return await res.json();
+  } 
+
+  const promises = selection.map(round => Promise.all([
+    load_details(round.a),
+    load_details(round.b)
+  ]));
+
+  let i = 0;
+
 </script>
 
 <header>
@@ -12,7 +24,23 @@
 </header>
 
 <div class="game-container">
-  <p>game will go here...</p>
+  {#await promises[i] then [a,b]}
+    <div class="game">
+      <div class="card-container">
+        {a.name}
+      </div>
+      <div>
+        <button class="same">
+          Same Price
+        </button>
+      </div>
+      <div class="card-container">
+        {b.name}
+      </div>
+    </div>
+  {:catch}
+    <p class="error">Failed to load the data!!</p>
+  {/await}
 </div>
 
 <div class="results">
@@ -22,5 +50,8 @@
 <style>
   .game-container {
     flex: 1
+  }
+  .error {
+    color: red;
   }
 </style>
