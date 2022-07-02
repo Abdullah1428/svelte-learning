18fbace103ada75a5305eaed643ae386c1ad5466
Abdullah1428 <abdullahcse1428@gmail.com>
2022-07-02 15:05:42 +0200

    started with game screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte'
 	import Welcome from './screens/Welcome.svelte'
+	import Game from './screens/Game.svelte'
 	import { select } from './select'
 
 	let celebs_promise;
@@ -49,7 +50,7 @@
 	{#if state === 'welcome'}
 		<Welcome on:select={start} />
 	{:else if state === 'playing'}
-		<p>game screen goes here</p>
+		<Game {selection} />
 	{/if}
 	
 </main>
---
+++ src/screens/Game.svelte
@@ -0,0 +1,26 @@
+<script>
+  // making the selection prop available that is passed from App.svelte
+  export let selection;
+
+  console.log(selection)
+</script>
+
+<header>
+  <p>
+    Tap on the more monetisable celebrity's face, or tap 'same price' if society values them equally.
+  </p>
+</header>
+
+<div class="game-container">
+  <p>game will go here...</p>
+</div>
+
+<div class="results">
+  <p>results will go here...</p>
+</div>
+
+<style>
+  .game-container {
+    flex: 1
+  }
+</style>
